431eac83a309a347c16e712839dd488c43be85e9
JAlvarezGar <jjalvarezgar@gmail.com>
2020-02-13 21:04:14 +0100

    algunos cambios en el layout del reproductor, añado botones de play, stop, pause y un logo

--- app/src/main/java/com/example/nuevoreproductormultimedia/Reproductor.java
+++ app/src/main/java/com/example/nuevoreproductormultimedia/Reproductor.java
@@ -3,12 +3,22 @@ package com.example.nuevoreproductormultimedia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class Reproductor extends AppCompatActivity {
+
+    ImageButton play,stop,pause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reproductor);
+
+        play=(ImageButton)findViewById(R.id.play);
+        stop=(ImageButton)findViewById(R.id.stop);
+        pause=(ImageButton)findViewById(R.id.pause);
+
+
+
     }
 }
